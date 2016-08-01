ea4719f6e78c8cc7293c0eadb7f8d6ef849bca30
Chris <chris@mymojo.ca>
2016-08-01 22:55:44 +0100

    Initial commit

---
+++ Chapter1/Chapter1.js
@@ -0,0 +1,30 @@
+﻿window.onload = function () {
+    queryById();
+    queryByElement();
+    queryByClass();
+
+    alterDom();
+}
+
+function queryById() {
+    var element = document.getElementById("outerDiv");
+    alert(element.innerHTML);
+    var queryElement = document.querySelector("#outerDiv");
+    alert(queryElement.innerHTML);
+}
+
+function queryByElement() {
+    var paragraphs = document.getElementsByTagName("p");
+    alert(paragraphs.length);
+    var queryParagraphs = document.querySelectorAll("p");
+    alert(queryParagraphs.length);
+}
+
+function queryByClass() {
+    var paragraphs = document.getElementsByClassName("subPara");
+    alert("<p> elements with class subPara:" + paragraphs.length);
+}
+
+function alterDom() {
+
+}
